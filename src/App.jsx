e4199360ac471c83eccc84c56ca1f95d7d9cd33b
Ana Bonaca <abonaca@gmail.com>
2023-04-12 23:26:46 -0700

    explore jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 
-const title = 'React';
+const title = 'React2';
+
+const welcome = {
+    greeting: 'Hey',
+    title: 'React',
+};
+
+function getTitle(title) {
+    return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <h1>{welcome.greeting} {welcome.title}!</h1>
+      <h2>Hello {getTitle('World')}!</h2>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
