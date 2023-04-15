4e653878ac1560064efd46fe880ad40bb5804855
Ana Bonaca <abonaca@gmail.com>
2023-04-15 01:01:41 -0700

    jsx list

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,54 @@ const welcome = {
     title: 'React',
 };
 
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    }
+    
+]
+
 function getTitle(title) {
     return title;
 }
 
 function App() {
-  return (
+    return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}!</h1>
-      <h2>Hello {getTitle('World')}!</h2>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+        <h1>My Hacker Stories</h1>
+        
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+        
+        <hr />
+        <ul>
+        {
+            list.map(function (item) {
+                return <li key={item.objectID}>
+                <span><a href={item.url}>{item.title}</a>: </span>
+                <span>{item.author} | </span>
+                <span>comments: {item.num_comments}, </span>
+                <span>points: {item.points}</span>
+                </li>
+            }
+            )
+        }
+        </ul>
     </div>
-  );
+    );
 }
 
 export default App;
